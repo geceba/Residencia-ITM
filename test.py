8ff881a0081d8daaf2a2a2bc4d0327b1a9bfe994
Gerardo Cetzal <gerardoce23@gmail.com>
2018-09-02 18:29:53 -0500

    test.py deleted

--- test.py
+++
@@ -1,5 +0,0 @@
-from alpha_vantage.timeseries import TimeSeries
-from pprint import pprint
-ts = TimeSeries(key='TU10HCWDTV5CNVBN', output_format='pandas')
-data, meta_data = ts.get_intraday(symbol='MSFT',interval='1min', outputsize='full')
-pprint(data.head(2))
